75736a49d62ef61df9a080dcf54fc62cd30c5e72
maicek_ <mmaaciek.dlugosz@outlook.com>
2023-09-07 08:37:16 +0200

    Feat(pages): Added typings for pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,16 +1,30 @@
 import { $fetch } from 'ofetch';
 import CONFIG from '../config';
 
-interface Options {}
+interface Options {
+  version?: 'production' | 'draft';
+}
+
+type PageContent = Record<string, number | string | object>;
+
+interface PageDocument<T extends PageContent = {}> {
+  id: string;
+  slug: string;
+  contentType: string;
+  content: T;
+}
 
 export const makePageEndpointUrl = (slug: string) => {
   return `${CONFIG.API_URL}/pages/${slug}`;
 };
 
-export const fetchPage = (slug: string, options: Options = {}) => {
+export const fetchPage = <T extends PageContent = {}>(
+  slug: string,
+  options: Options = {}
+) => {
   const url = makePageEndpointUrl(slug);
 
-  return $fetch(url, {
+  return $fetch<PageDocument<T>>(url, {
     method: 'GET',
     query: options,
   });
