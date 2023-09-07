cb8ae72123e16ecb04b5f9e9eeba1f1c028c2cbd
maicek_ <mmaaciek.dlugosz@outlook.com>
2023-09-07 10:06:23 +0200

    Removed process env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const CONFIG = {
-  API_URL: process?.env?.API_URL || 'https://api.twilify.app',
-  CDN_URL: process?.env?.CDN_URL || 'https://cdn.twilify.app',
-  DEBUG: process?.env?.DEBUG || false,
+  API_URL: 'https://api.twilify.app',
+  CDN_URL: 'https://cdn.twilify.app',
+  DEBUG: false,
 };
 
 export default CONFIG;
